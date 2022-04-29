fc0465fb55585d6daf8602fae87700493a2c5e5d
tetraxcode <abhinavreddy1961@gmail.com>
2022-04-29 09:27:14 -0700

    Initial Commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Abhinav Reddy on 4/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Abhinav Reddy on 4/29/22.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
